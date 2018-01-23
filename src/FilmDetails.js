4e3913f3f5c78fbd250101d5f10d74d00d35800f
jacquelynmarcella <jacquelyn.marcella@gmail.com>
2018-01-22 20:25:18 -0800

    Finished up cases for if there is no film selected; env api key is not working so still needing to manually paste into url when I run it still

--- src/FilmDetails.js
+++ src/FilmDetails.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class FilmDetails extends Component {
-	render() {
-	const backdropUrl = `https://image.tmdb.org/t/p/w1280/${this.props.films.backdrop_path}`
-	const posterUrl = `https://image.tmdb.org/t/p/w780/${this.props.films.poster_path}`
-	console.log(this.props);
-	console.log(backdropUrl);
+const FilmDetails = (props) => {
+
+	if(props.films.title) {
+		const backdropUrl = `https://image.tmdb.org/t/p/w1280/${props.films.backdrop_path}`
+		const posterUrl = `https://image.tmdb.org/t/p/w780/${props.films.poster_path}`
+		console.log(props);
+		console.log(backdropUrl);
 		return (
 			<div className="film-details">
          		<h1 className="section-title">DETAILS</h1>			
@@ -13,19 +14,28 @@ class FilmDetails extends Component {
 			<div className="film-detail is-hydrated">
  			 <figure className="film-backdrop">
     			<img src={backdropUrl} alt="" />
-    			<h1 className="film-title">{this.props.films.title}</h1>
+    			<h1 className="film-title">{props.films.title}</h1>
  				</figure>
 
  			 <div className="film-meta">
-    			<h2 className="film-tagline">{this.props.films.tagline}</h2>
+    			<h2 className="film-tagline">{props.films.tagline}</h2>
     			<p className="film-detail-overview">
-      				<img src={posterUrl} className="film-detail-poster" alt={this.props.films.title} />
-      				{this.props.films.overview}
+      				<img src={posterUrl} className="film-detail-poster" alt={props.films.title} />
+      				{props.films.overview}
    		 		</p>
   			</div>
 			</div>
 			</div>
-		);
+		)
+	} else {
+		return (
+			<div className="film-details">
+				<h1 className="section-title">DETAILS</h1>	
+         		<div className="film-detail">
+         			<p>No film selected</p>
+         		</div>
+			</div>
+		)
 	}
 }
 
